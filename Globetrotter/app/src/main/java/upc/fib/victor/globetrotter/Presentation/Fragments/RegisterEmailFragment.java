35808a38d694d9014b6dab3dd82a6aef2111570a
visape <vikktor.1996@hotmail.com>
2019-01-10 20:09:48 +0100

    añadir terminos y condiciones de uso

--- Globetrotter/app/src/main/java/upc/fib/victor/globetrotter/Presentation/Fragments/RegisterEmailFragment.java
+++ Globetrotter/app/src/main/java/upc/fib/victor/globetrotter/Presentation/Fragments/RegisterEmailFragment.java
@@ -1,5 +1,6 @@
 package upc.fib.victor.globetrotter.Presentation.Fragments;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -7,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import upc.fib.victor.globetrotter.R;
@@ -15,6 +19,12 @@ import upc.fib.victor.globetrotter.R;
 public class RegisterEmailFragment extends Fragment {
 
     private Button siguienteBtn;
+
+    private AlertDialog dialog;
+
+    private TextView terminosTxt;
+
+    private CheckBox checkBox;
 
     private EditText correoTxt;
     private EditText passwordTxt;
@@ -46,26 +56,56 @@ public class RegisterEmailFragment extends Fragment {
         passwordTxt = view.findViewById(R.id.passwordTxt);
         passwordTxt2 = view.findViewById(R.id.password2Txt);
         siguienteBtn = view.findViewById(R.id.siguiente_btn);
+        terminosTxt = view.findViewById(R.id.linkTerminos);
+        checkBox = view.findViewById(R.id.terminos);
+
+        terminosTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                LayoutInflater inflater = getLayoutInflater();
+
+                View v = inflater.inflate(R.layout.dialog_terminos_y_condiciones, null);
+                builder.setView(v);
+
+                ImageView closeBtn = v.findViewById(R.id.closeBtn);
+                closeBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog = builder.create();
+                dialog.show();
+            }
+        });
 
         siguienteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String correo = correoTxt.getText().toString().trim();
-                if (correo.equals("") || !correo.contains("@")) {
-                    Toast.makeText(getContext(), "Correo electrónico no válido",
-                            Toast.LENGTH_SHORT).show();
-                } else {
-                    String password = passwordTxt.getText().toString().trim();
-                    String password2 = passwordTxt2.getText().toString().trim();
-                    if (password.length() < 8) {
-                        Toast.makeText(getContext(), "La contraseña debe tener más de 8 caracteres",
-                                Toast.LENGTH_SHORT).show();
-                    } else if (!password.equals(password2)) {
-                        Toast.makeText(getContext(), "Las contraseñas deben coincidir",
+                if (checkBox.isChecked()) {
+                    String correo = correoTxt.getText().toString().trim();
+
+                    if (correo.equals("") || !correo.contains("@")) {
+                        Toast.makeText(getContext(), "Correo electrónico no válido",
                                 Toast.LENGTH_SHORT).show();
                     } else {
-                        mListener.onSetCorreo(correo, password);
+                        String password = passwordTxt.getText().toString().trim();
+                        String password2 = passwordTxt2.getText().toString().trim();
+                        if (password.length() < 8) {
+                            Toast.makeText(getContext(), "La contraseña debe tener más de 8 caracteres",
+                                    Toast.LENGTH_SHORT).show();
+                        } else if (!password.equals(password2)) {
+                            Toast.makeText(getContext(), "Las contraseñas deben coincidir",
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            mListener.onSetCorreo(correo, password);
+                        }
                     }
+                } else {
+                    Toast.makeText(getContext(), "Debes aceptar los términos y condiciones",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
